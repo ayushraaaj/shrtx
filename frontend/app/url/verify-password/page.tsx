0864dbba33049373f10c4e84441daf6e41b338c5
Ayush Raj <ayushrajar000@gmail.com>
2026-02-05 22:15:46 +0530

    minor suspense component change

--- frontend/app/url/verify-password/page.tsx
+++ frontend/app/url/verify-password/page.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense, useState } from "react";
 
-const UrlPassword = () => {
+const UrlPasswordContent = () => {
     const searchParams = useSearchParams();
 
     const shortCode = searchParams.get("shortCode");
@@ -40,46 +40,56 @@ const UrlPassword = () => {
     };
 
     return (
-        <Suspense>
-            <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-51 p-4">
-                <div className="bg-white w-96 rounded-xl p-6 shadow-lg">
-                    <h2 className="text-lg font-bold mb-4">Link Password</h2>
+        <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center z-51 p-4">
+            <div className="bg-white w-96 rounded-xl p-6 shadow-lg">
+                <h2 className="text-lg font-bold mb-4">Link Password</h2>
 
-                    <div className="mb-4">
-                        <input
-                            type="password"
-                            placeholder="Enter password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value.trim())}
-                            className={`w-full border rounded p-2 ${
-                                error ? "border-red-500 bg-red-50" : ""
-                            }`}
-                        />
+                <div className="mb-4">
+                    <input
+                        type="password"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value.trim())}
+                        className={`w-full border rounded p-2 ${
+                            error ? "border-red-500 bg-red-50" : ""
+                        }`}
+                    />
 
-                        {error && (
-                            <p className="text-red-500 text-xs mt-1 font-medium animate-pulse">
-                                {error}
-                            </p>
-                        )}
-                    </div>
+                    {error && (
+                        <p className="text-red-500 text-xs mt-1 font-medium animate-pulse">
+                            {error}
+                        </p>
+                    )}
+                </div>
 
-                    <div className="flex justify-end gap-3">
-                        {/* <button
+                <div className="flex justify-end gap-3">
+                    {/* <button
                         // onClick={closeUrlPasswordModal}
                         className="px-4 py-2 border rounded"
                     >
                         Cancel
                     </button> */}
 
-                        <button
-                            onClick={onDone}
-                            className="px-4 py-2 bg-blue-600 text-white rounded"
-                        >
-                            Done
-                        </button>
-                    </div>
+                    <button
+                        onClick={onDone}
+                        className="px-4 py-2 bg-blue-600 text-white rounded"
+                    >
+                        Done
+                    </button>
                 </div>
             </div>
+        </div>
+    );
+};
+
+const UrlPassword = () => {
+    return (
+        <Suspense
+            fallback={
+                <div className="fixed inset-0 bg-black/10 backdrop-blur-sm" />
+            }
+        >
+            <UrlPasswordContent />
         </Suspense>
     );
 };
