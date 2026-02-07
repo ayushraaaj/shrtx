e46bd21df9bdcf0593b5c00b9d767acba4e89fb8
Ayush Raj <ayushrajar000@gmail.com>
2026-02-07 18:53:21 +0530

    minor change in axios.ts

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://localhost:8000/api/v1",
-    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`,
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}api/v1`,
     withCredentials: true,
 });
